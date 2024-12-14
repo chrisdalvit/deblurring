fec376272457289ed0718890ba56d0408f817276
Christian Dalvit <chris.dalvit@gmail.com>
2024-12-14 17:11:01 +0100

    remove broken data download script

--- data/downlaod.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-url = 'https://drive.usercontent.google.com/download?id=1y4wvPdOG3mojpFCHTqLgriexhbjoWVkK&export=download&authuser=0'
-output = 'GOPRO_download.tgz'
-gdown.download(url, output, quiet=False)
