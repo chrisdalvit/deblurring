450b222934c14a2b0384600d5bcedae6bec8ac88
Christian Dalvit <chris.dalvit@gmail.com>
2024-12-14 17:46:54 +0100

    add command line arguments

--- src/main.py
+++ src/main.py
@@ -1,36 +1,34 @@
+import argparse
 import torch
-from PIL import Image
-from torch.optim.lr_scheduler import CosineAnnealingLR
 from model import DDANet
-from utils import get_transforms, loss_fn
+from utils import loss_fn
 from data import train_dataloader
 
-img = Image.open("./data/sample.jpg")
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type=int)
+parser.add_argument("--lr_start", type=float, default=1e-4)
+parser.add_argument("--lr_min", type=float, default=1e-6)
 
-transforms = get_transforms()
-X = transforms(img).unsqueeze(0)
-
-dda = DDANet(
-    in_channels=3,
-    hid_channels=32,
-    kernel_size=3,
-    sam_groups=4,
-    attention_size=3
-)
-
-lr = 1e-4
-min_lr = 1e-6
-epochs = 2
-test_dataloader = train_dataloader("./data/GOPRO", batch_size=4)
-optimizer = torch.optim.Adam(dda.parameters(), lr=lr)
-schedule = CosineAnnealingLR(optimizer, len(test_dataloader), eta_min=min_lr)
-
-dda.train()
-for epoch in range(epochs):
-    for X, y in test_dataloader:
-        outputs = dda(X)
-        loss = loss_fn(outputs, y)
-        loss.backward()
-        optimizer.step()
-        print(loss)
-    schedule.step()
+def main():
+    args = parser.parse_args()    
+    test_dataloader = train_dataloader("./data/GOPRO", batch_size=4)
+    dda = DDANet(
+        in_channels=3,
+        hid_channels=32,
+        kernel_size=3,
+        sam_groups=8,
+        attention_size=3
+    )
+    optimizer = torch.optim.Adam(dda.parameters(), lr=args.lr)
+    schedule = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(test_dataloader), eta_min=args.min_lr)
+    dda.train()
+    for epoch in range(args.epochs):
+        epoch_loss = 0
+        for X, y in test_dataloader:
+            outputs = dda(X)
+            loss = loss_fn(outputs, y)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss
+        schedule.step()
+        print(loss / len(test_dataloader))
